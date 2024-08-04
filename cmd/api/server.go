f8e8dbece565209f3a3c889f5906600dfc810142
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-08-04 13:45:35 +0630

    feat(app): handle graceful shutdown using channel, goroutine and context

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func (app *application) serve() error {
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
@@ -17,10 +22,41 @@ func (app *application) serve() error {
 
 	app.logger.Printf("Server is starting at %d", app.config.port)
 
+	// handle graceful shutdown using channel and goroutine
+	shutdownErr := make(chan error, 1)
+	go func() {
+		relay := make(chan os.Signal, 1)
+		signal.Notify(relay, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+		s := <-relay
+		app.logger.Println("Received signal:", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownErr <- err
+		}
+
+		app.logger.Println("Starting to clean up goroutines.")
+		app.wg.Wait()
+		shutdownErr <- err
+	}()
+
 	err := srv.ListenAndServe()
 
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	app.logger.Println("The server has just stopped now.")
+
+	err = <-shutdownErr
 	if err != nil {
 		return err
 	}
+
+	app.logger.Println("The server has completely stopped.")
+
 	return nil
 }
